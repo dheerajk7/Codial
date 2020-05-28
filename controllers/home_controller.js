bd299ea22e347bc26d6b84ea5fceea3fe3918998
Dheeraj <Dheeraj77253@gmail.com>
2020-05-28 16:07:26 +0530

    add post option and add a post into DB also display them on home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,31 @@
+const Post = require('../models/post');
+
 module.exports.home = function(request,response)
 {
     // console.log(request.cookies);       //accessing cookie
     // response.cookie('user_id','45');     //updating cookie
-    return response.render('home',{
-        title:'Home | Codial',
+
+    // Post.find({},function(err,posts)
+    // {
+    //     if(err){console.log("Error in fetching post");return;}
+    //     return response.render('home',
+    //     {
+    //         title:'Home | Codial',
+    //         posts:posts,
+    //     })
+    // });
+
+    //populate user of each post
+    Post.find({}).populate('user').exec(function(request,response)
+    {
+        if(err){console.log("Error in fetching post");return;}
+        return response.render('home',
+        {
+            title:'Home | Codial',
+            posts:posts,
+        })
     });
-};
+}
 
 module.exports.contactUs = function(request,response)
 {
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -5,11 +5,11 @@ module.exports.addPost = function(request,response)
     Post.create(
         {
             content:request.body.content,
-            user:request.user,
+            user:request.user._id,
         },
         function(err,post){
             if(err){console.log('Error in adding Post');return response.redirect('back');}
             console.log('Post Added Successfully');
-            return response.redirect('/users/profile');
+            return response.redirect('/');
         });
 }
